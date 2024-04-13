f304113853cc0bf37eb75f1216a2cf0f6e8725c7
Kevin <k.vandenhoogenhof@student.fontys.nl>
2024-04-13 13:53:51 +0200

    Update script.js

--- Load-testing/scripts/script.js
+++ Load-testing/scripts/script.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 const isNumeric = (value) => /^\d+$/.test(value);
 
-const default_vus = 5;
+const default_vus = 1000;
 
 const target_vus_env = `${__ENV.TARGET_VUS}`;
 const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
@@ -11,19 +11,19 @@ const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_
 export let options = {
   insecureSkipTLSVerify: true,
   stages: [
-      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
-      { duration: '5s', target: target_vus },
+      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 30s
+      { duration: '30s', target: target_vus },
 
-      // Stay at rest on TARGET_VUS VUs for 10s
-      { duration: '10s', target: target_vus },
+      // Stay at rest on TARGET_VUS VUs for 1m30s
+      { duration: '1m30s', target: target_vus },
 
-      // Ramp-down from TARGET_VUS to 0 VUs for 5s
-      { duration: '5s', target: 0 }
+      // Ramp-down from TARGET_VUS to 0 VUs for 20s
+      { duration: '20s', target: 0 }
   ]
 };
 
 export default function () {
-  const res = http.get('https://host.docker.internal:9001/Thread/6602c643de4335977f479b3b'); //, {headers: {Accepts: 'application/json'}});
+  const res = http.get('https://host.docker.internal:5001/Thread/Thread'); //, {headers: {Accepts: 'application/json'}});
   check(res, { 'status is 200': (r) => r.status === 200 });
   sleep(1);
 };
